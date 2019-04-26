61ea76aaad27dd68826caf2c41e22f3bd79e8a16
Francisco Arencibia <arencibia.francisco@gmail.com>
2019-04-26 15:01:13 +0200

    modified:   ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,17 +1,21 @@
+#Entrada de datos
 a= int(input("Entre el primer número "))
 if type(a) != int:
     raise TypeError       
-
-a= a/10
 b= int(input ("Entre el segundo número "))
 if type(b) != int:
     raise TypeError
+
+#Procesamiento de datos
+a= a/10
 b=b/10
 suma = a+b
 resta = a-b
 multiplicacion = a*b
 division = a/b
-print(str(a) + ' + ' + str(b) + ' = ' + str(suma))
+
+#Salida de resultados
+print(str(a) , ' + ' , str(b) + ' = ' + str(suma))
 print(str(a) + ' - ' + str(b) + ' = ' + str(resta))
 print(str(a) + ' * ' + str(b) + ' = ' + str(multiplicacion))
 print(str(a) + ' / ' + str(b) + ' = ' + str(division))
